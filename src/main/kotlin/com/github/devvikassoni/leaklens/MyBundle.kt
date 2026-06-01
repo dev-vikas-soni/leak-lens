c1cd38f5275d4d707a77f2491974e6ff490c1c9c
GitHub Action <action@github.com>
2026-06-01 16:38:02 +0000

    Template cleanup

--- src/main/kotlin/com/github/devvikassoni/leaklens/MyBundle.kt
+++ src/main/kotlin/com/github/devvikassoni/leaklens/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.devvikassoni.leaklens
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/devvikassoni/leaklens/toolWindow/LeakPilotToolWindowFactory.kt
+++ src/main/kotlin/com/github/devvikassoni/leaklens/toolWindow/LeakPilotToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.devvikassoni.leaklens.toolWindow
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow

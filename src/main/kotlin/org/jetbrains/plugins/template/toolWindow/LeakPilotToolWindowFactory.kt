50461ebcbd4fa2f9b7f7f55af62da80f5abe513d
vikassoni <vikasacsoni9211@gmail.com>
2026-06-01 22:07:15 +0530

    Initial commit for Leak Lens: rename tool window and update plugin configuration

---
+++ src/main/kotlin/org/jetbrains/plugins/template/toolWindow/LeakPilotToolWindowFactory.kt
@@ -0,0 +1,27 @@
+package org.jetbrains.plugins.template.toolWindow
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowFactory
+import javax.swing.JLabel
+import javax.swing.JPanel
+
+class LeakPilotToolWindowFactory : ToolWindowFactory {
+
+    override fun createToolWindowContent(
+        project: Project,
+        toolWindow: ToolWindow
+    ) {
+        val panel = JPanel()
+
+        panel.add(
+            JLabel("LeakPilot is alive 🚀")
+        )
+
+        val content =
+            toolWindow.contentManager.factory
+                .createContent(panel, "", false)
+
+        toolWindow.contentManager.addContent(content)
+    }
+}
